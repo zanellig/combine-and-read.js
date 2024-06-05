671e9c45fb5b394922fd2c22db0611ba2e1800d6
Gonzalo Zanelli <gonzalozanelli1@gmail.com>
2024-06-05 15:45:42 -0300

    refactor: cambiar output path para que no se sobreescriba cuando cambiamos el CC

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const fs = require("fs");
-const path = require("path");
-const Tesseract = require("tesseract.js");
+const fs = require('fs');
+const path = require('path');
+const Tesseract = require('tesseract.js');
 
-const CC = "5017";
+const CC = '5017';
 
-const folderPath = path.join(__dirname, "images", CC);
-const outputPath = "output.txt";
+const folderPath = path.join(__dirname, 'images', CC);
+const outputPath = `${CC}-output.txt`;
 
 // Función para procesar una imagen y extraer texto usando Tesseract.js
 async function processImage(filePath) {
   try {
-    const result = await Tesseract.recognize(filePath, "eng", {
-      logger: (m) => console.log(m),
+    const result = await Tesseract.recognize(filePath, 'eng', {
+      logger: m => console.log(m),
     });
 
     // Suponiendo que el texto extraído está en el formato 'ID Nombre'
     const lines = result.data.text
-      .split("\n")
-      .map((line) => line.trim())
-      .filter((line) => line);
-    return lines.map((line) => {
-      const [id, ...nameParts] = line.split(" ");
-      const name = nameParts.join(" ");
+      .split('\n')
+      .map(line => line.trim())
+      .filter(line => line);
+    return lines.map(line => {
+      const [id, ...nameParts] = line.split(' ');
+      const name = nameParts.join(' ');
       const validId = id.length <= 4 ? id : id.slice(0, 4);
       return `${validId}\t${name}`;
     });
@@ -34,7 +34,7 @@ async function processImage(filePath) {
 // Leer todos los archivos en la carpeta
 fs.readdir(folderPath, async (err, files) => {
   if (err) {
-    console.error("Error leyendo la carpeta de imágenes:", err);
+    console.error('Error leyendo la carpeta de imágenes:', err);
     return;
   }
 
@@ -51,12 +51,12 @@ fs.readdir(folderPath, async (err, files) => {
   }
 
   // Convertir el array a string con saltos de línea y escribir en output.txt
-  const data = dataArray.join("\n");
-  fs.writeFile(outputPath, data, (err) => {
+  const data = dataArray.join('\n');
+  fs.writeFile(outputPath, data, err => {
     if (err) {
-      console.error("Error escribiendo el archivo de salida:", err);
+      console.error('Error escribiendo el archivo de salida:', err);
       return;
     }
-    console.log("Datos transcritos exitosamente a output.txt");
+    console.log(`Datos transcritos exitosamente a ${outputPath}`);
   });
 });
