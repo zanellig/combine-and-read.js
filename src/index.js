511e0109976704382e55c9edb3c1d3e034c0320f
Gonzalo Zanelli <gonzalozanelli1@gmail.com>
2024-06-10 16:40:33 -0300

    fix: add exception handling if tiendas arr is empty

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const { TIENDAS } = require('./tiendas');
 const ProcessManager = require('./ProcessManager');
 
 (async () => {
+  if (TIENDAS.length === 0) {
+    throw new Error('No hay tiendas para procesar');
+  }
   const manager = new ProcessManager(TIENDAS);
   await manager.processAll();
 })();
